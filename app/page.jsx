37f75ed7d8ebe363865c8e8b60fa412786a5495c
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-07-30 16:44:40 +0100

    added stuff

--- app/page.jsx
+++ app/page.jsx
@@ -8,18 +8,20 @@ export default function Home() {
     <section className="w-full flex flex-col items-center pt-20">
       <div className="w-[50%]">
         <h1 className="text-7xl -font-mona-sans font-semibold text-neutral-700">
-          Welcome, to my <br /> Minimalist Blog.
+          Welcome, <br /> to my Minimalist Blog.
         </h1>
       </div>
-      <div className="w-[50%] pt-10">
+      <div className="w-[50%] pt-30">
         <h2 className="text-4xl text-neutral-500 -font-mona-sans">
           Latest posts.
         </h2>
         <hr className="w-[50%] text-neutral-700" />
-        {posts &&
-          Object.entries(posts).map(([category, categoryPosts]) => (
-            <Post key={category} category={category} posts={categoryPosts} />
-          ))}
+        <section className="w-full pt-10 -font-poppins">
+          {posts &&
+            Object.entries(posts).map(([category, categoryPosts]) => (
+              <Post key={category} category={category} posts={categoryPosts} />
+            ))}
+        </section>
       </div>
     </section>
   );
--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -4,15 +4,16 @@ import Link from "next/link";
 const Post = ({ category, posts }) => {
   return (
     <div className="w-full mb-8">
-      <h2 className="text-2xl font-semibold mb-4">{category}</h2>
-      <div className="w-full flex flex-col gap-2">
+      <h2 className="text-3xl  font-semibold text-neutral-700 pb-2">
+        {" "}
+        {category}
+      </h2>
+      <div className="w-full flex gap-2">
         {posts?.map((post) => (
-          <Link
-            href={`/${post.id}`}
-            key={post.id}
-            className="hover:text-blue-500 transition-colors"
-          >
-            {post.title}
+          <Link href={`/${post.id}`} key={post.id} className="w-[40%]">
+            <p className="-font-mona-sans font-semibold text-neutral-600 hover:text-neutral-400">
+              {post.title}
+            </p>
           </Link>
         ))}
       </div>
