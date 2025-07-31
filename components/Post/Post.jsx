afa0abe6cf840b61624166aa0d27113d4cf2781f
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-07-31 13:33:01 +0100

    added

--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 const Post = ({ category, posts }) => {
   return (
     <div className="w-full mb-8">
-      <h2 className="text-3xl  font-semibold text-neutral-700 pb-2">
+      <h2 className="text-2xl  font-semibold text-neutral-700 pb-2">
         {" "}
         {category}
       </h2>
-      <div className="w-full flex gap-2">
+      <div className="w-full flex gap-2 justify-center">
         {posts?.map((post) => (
-          <Link href={`/${post.id}`} key={post.id} className="w-[40%]">
+          <Link href={`/${post.id}`} key={post.id} className="w-[20%]">
             <p className="-font-mona-sans font-semibold text-neutral-600 hover:text-neutral-400">
               {post.title}
             </p>
