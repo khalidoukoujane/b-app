844815ee616e30f634bc100e5c259a0e0b8b5231
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-07-31 13:31:03 +0100

    added

---
+++ app/[slug]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+	<div>
+	  this is lol
+	</div>
+  )
+}
+
+export default page
--- app/page.jsx
+++ app/page.jsx
@@ -5,23 +5,22 @@ import getCategorisedPosts from "@/lib/posts";
 export default function Home() {
   const posts = getCategorisedPosts();
   return (
-    <section className="w-full flex flex-col items-center pt-20">
-      <div className="w-[50%]">
+    <section className="w-full flex flex-col items-center justify-center pt-20">
+      <div className="flex flex-col items-center text-center">
         <h1 className="text-7xl -font-mona-sans font-semibold text-neutral-700">
-          Welcome, <br /> to my Minimalist Blog.
+          <span className="text-neutral-500"> Welcome,</span> <br /> to my
+          Minimalist Blog.
         </h1>
-      </div>
-      <div className="w-[50%] pt-30">
-        <h2 className="text-4xl text-neutral-500 -font-mona-sans">
-          Latest posts.
-        </h2>
-        <hr className="w-[50%] text-neutral-700" />
-        <section className="w-full pt-10 -font-poppins">
-          {posts &&
-            Object.entries(posts).map(([category, categoryPosts]) => (
-              <Post key={category} category={category} posts={categoryPosts} />
-            ))}
-        </section>
+        <div className="mt-10 w-auto">
+          <Image
+            src={"/main-img2.webp"}
+            width={800}
+            height={800}
+            alt="main img"
+            layout="responsive"
+            className="rounded-2xl"
+          />
+        </div>
       </div>
     </section>
   );
