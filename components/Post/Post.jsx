c511c315e39428d6726374ebb9be0ff37d8920ed
khalidoukoujane <oukoujanekhalid9@gmail.com>
2025-08-17 14:44:24 +0100

    added

--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Post = ({ category, posts }) => {
   const truncateTitle = (title) => {
@@ -12,13 +13,11 @@ const Post = ({ category, posts }) => {
 
   return (
     <div className="w-full mb-8">
-      <h2 className="text-3xl font-semibold text-[#495057] pb-2">
-        {category}
-      </h2>
+      <h2 className="text-3xl font-semibold text-[#495057] pb-2">{category}</h2>
       <div className="w-full flex flex-col md:flex-row gap-10 md:items-center items-start text-xl">
         {posts?.map((post, i) => (
-          <div key={i} className="w-full h-[400px] bg-slate-200 rounded-[20px]">
-
+          <div key={i} className="w-full h-[400px] bg-[#f8f9fa] rounded-[20px] overflow-hidden border-[2px] border-[##212529]">
+            <img src={post.img} alt="post image" className="w-full h-[50%] object-cover" />
           </div>
         ))}
       </div>
